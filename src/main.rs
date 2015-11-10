3741cdfec110f0705fcc633f57545d25e1b50633
Justin Browne <jebdude89@gmail.com>
2015-11-10 18:11:26 -0500

    Add command line options

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+extern crate getopts;
+
 mod nucleus;
 mod color;
 
-use std::io::{BufReader,BufRead,Write};
+use std::io::{BufReader, BufRead, Write};
 use std::fs::File;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::env;
+use getopts::Options;
 use nucleus::Nucleus;
 use color::Color;
 
-fn get_col(fname: String) -> HashMap<String,Color> {
+fn get_col(fname: String) -> HashMap<String, Color> {
     let mut col = HashMap::new();
     let f = BufReader::new(File::open(fname).unwrap());
     for l in f.lines() {
@@ -23,7 +27,7 @@ fn get_col(fname: String) -> HashMap<String,Color> {
     col
 }
 
-fn get_nucl(fname: String) -> HashMap<String,Nucleus> {
+fn get_nucl(fname: String) -> HashMap<String, Nucleus> {
     let mut nucl = HashMap::new();
     let f = BufReader::new(File::open(fname).unwrap());
     for l in f.lines() {
@@ -38,7 +42,7 @@ fn get_nucl(fname: String) -> HashMap<String,Nucleus> {
     nucl
 }
 
-fn get_elem(fname: String) -> HashMap<u8,String> {
+fn get_elem(fname: String) -> HashMap<u8, String> {
     let mut elem = HashMap::new();
     let f = BufReader::new(File::open(fname).unwrap());
     for l in f.lines() {
@@ -53,7 +57,7 @@ fn get_elem(fname: String) -> HashMap<u8,String> {
     elem
 }
 
-fn get_nuccol(fname: String) -> HashMap<String,String> {
+fn get_nuccol(fname: String) -> HashMap<String, String> {
     let mut nuccol = HashMap::new();
     let f = BufReader::new(File::open(fname).unwrap());
     for l in f.lines() {
@@ -82,61 +86,66 @@ fn get_magic(fname: String) -> HashSet<u8> {
     magic
 }
 
-fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, col: &HashMap<String,Color>, elem: &HashMap<u8,String>, magic: &HashSet<u8>) {
-    let mut z_limits = HashMap::<u8,(u8,u8)>::new();
-    let mut n_limits = HashMap::<u8,(u8,u8)>::new();
-    let mut chart_z: Option<(u8,u8)> = None;
-    let mut chart_n: Option<(u8,u8)> = None;
+fn output_svg(nucl: &HashMap<String, Nucleus>,
+              nuccol: &HashMap<String, String>,
+              col: &HashMap<String, Color>,
+              elem: &HashMap<u8, String>,
+              magic: &HashSet<u8>) {
+    let mut z_limits = HashMap::<u8, (u8, u8)>::new();
+    let mut n_limits = HashMap::<u8, (u8, u8)>::new();
+    let mut chart_z: Option<(u8, u8)> = None;
+    let mut chart_n: Option<(u8, u8)> = None;
     let scale = 10;
 
     // Determine the limits of the chart
-    for (_,n) in nucl {
+    for (_, n) in nucl {
         if chart_z == None {
-            chart_z = Some((n.z,n.z));
+            chart_z = Some((n.z, n.z));
         } else {
             let z0 = chart_z.unwrap().0;
             let z1 = chart_z.unwrap().1;
             if n.z < z0 {
-                chart_z = Some((n.z,z1));
+                chart_z = Some((n.z, z1));
             } else if n.z > z1 {
-                chart_z = Some((z0,n.z));
+                chart_z = Some((z0, n.z));
             }
         }
         if chart_n == None {
-            chart_n = Some((n.n,n.n));
+            chart_n = Some((n.n, n.n));
         } else {
             let n0 = chart_n.unwrap().0;
             let n1 = chart_n.unwrap().1;
             if n.n < n0 {
-                chart_n = Some((n.n,n1));
+                chart_n = Some((n.n, n1));
             } else if n.n > n1 {
-                chart_n = Some((n0,n.n));
+                chart_n = Some((n0, n.n));
             }
         }
     }
 
-    // Determine the lowest and highest Z for each N and lowest and highest N for each Z
-    for (_,n) in nucl {
-        let x = z_limits.entry(n.z).or_insert((n.n,n.n));
+    // Determine the lowest and highest Z for each N and lowest and highest N for
+    // each Z
+    for (_, n) in nucl {
+        let x = z_limits.entry(n.z).or_insert((n.n, n.n));
         if n.n < x.0 {
-            *x = (n.n,x.1);
+            *x = (n.n, x.1);
         } else if n.n > x.1 {
-            *x = (x.0,n.n);
+            *x = (x.0, n.n);
         }
 
-        let x = n_limits.entry(n.n).or_insert((n.z,n.z));
+        let x = n_limits.entry(n.n).or_insert((n.z, n.z));
         if n.z < x.0 {
-            *x = (n.z,x.1);
+            *x = (n.z, x.1);
         } else if n.z > x.1 {
-            *x = (x.0,n.z);
+            *x = (x.0, n.z);
         }
     }
 
     // Output the SVG
     let mut svgfile = File::create("out.svg").unwrap();
     // Header
-    let w = ((chart_n.unwrap().1 as u32)-(chart_n.unwrap().0 as u32)+4)*scale;
-    let h = ((chart_z.unwrap().1 as u32)-(chart_z.unwrap().0 as u32)+3)*scale;
+    let w = ((chart_n.unwrap().1 as u32) - (chart_n.unwrap().0 as u32) + 4) * scale;
+    let h = ((chart_z.unwrap().1 as u32) - (chart_z.unwrap().0 as u32) + 3) * scale;
     let _ = write!(svgfile, "<svg xmlns=\"http://www.w3.org/2000/svg\"");
     let _ = write!(svgfile, " xmlns:xlink=\"http://www.w3.org/1999/xlink\"");
     let _ = write!(svgfile, " width=\"{}\" height=\"{}\">\n", w, h);
@@ -145,19 +154,24 @@ fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, c
     let _ = write!(svgfile, "<style>\n");
     let _ = write!(svgfile, ".nucBox{{stroke:black;stroke-width:.1;}}\n");
     let _ = write!(svgfile, ".elName{{text-anchor:end;}}\n");
-    let _ = write!(svgfile, ".magBox{{fill:none;stroke:black;stroke-width:.25;}}\n");
+    let _ = write!(svgfile,
+                   ".magBox{{fill:none;stroke:black;stroke-width:.25;}}\n");
 
-    for (name,c) in col {
+    for (name, c) in col {
         let _ = write!(svgfile, ".{}{{fill:rgb({},{},{});}}\n", name, c.r, c.g, c.b);
     }
 
     let _ = write!(svgfile, "</style>\n");
 
     // Create Transform Group
-    let _ = write!(svgfile, "<g transform=\"scale({}) translate({},{}) scale(1,-1)\">\n", scale, 2-(chart_n.unwrap().0 as i32), (chart_z.unwrap().1 as i32)+2);
+    let _ = write!(svgfile,
+                   "<g transform=\"scale({}) translate({},{}) scale(1,-1)\">\n",
+                   scale,
+                   2 - (chart_n.unwrap().0 as i32),
+                   (chart_z.unwrap().1 as i32) + 2);
 
     // Nuclide Boxes
-    for (name,n) in nucl {
+    for (name, n) in nucl {
         let x = n.n;
         let y = n.z;
         if let Some(c) = nuccol.get(name) {
@@ -168,12 +182,12 @@ fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, c
     }
 
     // Element Symbols
-    for (z,e) in elem {
+    for (z, e) in elem {
         // Determine x position
         // Only include element symbol if one of its isotopes is included
         if let Some(z1) = z_limits.get(&z) {
             let mut x = z1.0;
-            if let Some(z2) = z_limits.get(&(z+1)) {
+            if let Some(z2) = z_limits.get(&(z + 1)) {
                 if z2 < z1 {
                     x = z2.0;
                 }
@@ -184,7 +198,10 @@ fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, c
             let xoff = -0.25;
             let yoff = -0.25;
             let _ = write!(svgfile, "<text x=\"{}\" y=\"{}\"", xoff, yoff);
-            let _ = write!(svgfile, " transform=\"translate({},{}) scale(1,-1)\"", xpos, ypos);
+            let _ = write!(svgfile,
+                           " transform=\"translate({},{}) scale(1,-1)\"",
+                           xpos,
+                           ypos);
             let _ = write!(svgfile, " font-size=\".9\" class=\"elName\">");
             let _ = write!(svgfile, "{}", e);
             let _ = write!(svgfile, "</text>\n");
@@ -197,7 +214,7 @@ fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, c
         if let Some(nl) = n_limits.get(&m) {
             let xpos = m;
             let ypos = nl.0;
-            let h = nl.1-nl.0+1;
+            let h = nl.1 - nl.0 + 1;
             let _ = write!(svgfile, "<rect x=\"{}\" y=\"{}\"", xpos, ypos);
             let _ = write!(svgfile, " width=\"1\" height=\"{}\"", h);
             let _ = write!(svgfile, " class=\"magBox\" />\n");
@@ -206,7 +223,7 @@ fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, c
         if let Some(zl) = z_limits.get(&m) {
             let xpos = zl.0;
             let ypos = m;
-            let w = zl.1-zl.0+1;
+            let w = zl.1 - zl.0 + 1;
             let _ = write!(svgfile, "<rect x=\"{}\" y=\"{}\"", xpos, ypos);
             let _ = write!(svgfile, " width=\"{}\" height=\"1\"", w);
             let _ = write!(svgfile, " class=\"magBox\" />\n");
@@ -218,11 +235,50 @@ fn output_svg(nucl: &HashMap<String,Nucleus>, nuccol: &HashMap<String,String>, c
 }
 
 fn main() {
-    let nucl = get_nucl("data/nuclei".to_string());
-    let nuccol = get_nuccol("data/nuccol".to_string());
-    let col = get_col("data/colors".to_string());
-    let elem = get_elem("data/elements".to_string());
-    let magic = get_magic("data/magic".to_string());
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+
+    opts.optopt("o",
+                "output",
+                "The SVG output file",
+                "FILE");
+    opts.optopt("n",
+                "nuclei",
+                "The data file containing nuclide information",
+                "FILE");
+    opts.optopt("u",
+                "nuccol",
+                "The data file containing nuclide-color map",
+                "FILE");
+    opts.optopt("c",
+                "colors",
+                "The data file containing color definitions",
+                "FILE");
+    opts.optopt("e",
+                "elements",
+                "The data file containing element information",
+                "FILE");
+    opts.optopt("m",
+                "magic",
+                "The data file containing magic number information",
+                "FILE");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    let nucl_fname = matches.opt_str("n").unwrap_or("data/nuclei".to_string());
+    let nuccol_fname = matches.opt_str("u").unwrap_or("data/nuccol".to_string());
+    let col_fname = matches.opt_str("c").unwrap_or("data/colors".to_string());
+    let elem_fname = matches.opt_str("e").unwrap_or("data/elements".to_string());
+    let magic_fname = matches.opt_str("m").unwrap_or("data/magic".to_string());
+
+    let nucl = get_nucl(nucl_fname);
+    let nuccol = get_nuccol(nuccol_fname);
+    let col = get_col(col_fname);
+    let elem = get_elem(elem_fname);
+    let magic = get_magic(magic_fname);
 
     output_svg(&nucl, &nuccol, &col, &elem, &magic);
 }
